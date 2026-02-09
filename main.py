2cfc0e1e505ec8b30a0402ccef24daca97449886
lau-kai-rui-gavin@s2023.ssts.edu.sg <157783000+LauKRGavin@users.noreply.github.com>
2026-02-09 22:53:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1082,9 +1082,9 @@ with tab6:
    st.subheader(f"**Hours of work done:** {total_hours_completed:.1f}h,**Total activities:** {total_activities} Activites,**Badges Earnt:** {Badge} Badges")
    col1, col2, col3 = st.columns(3)
    with col1:
-      st.markdown("<h1 style='text-align: center; margin-top: 5rem; color: #FFFFFF;'>✅", unsafe_allow_html=True)
+      st.markdown("<h1 style='text-align: center; margin-bottom: 20rem; color: #FFFFFF;'>✅", unsafe_allow_html=True)
       if total_hours_completed >= 0:
-         st.markdown("<h1 style='text-align: center; margin-top: 1rem; color: #FFFFFF;'> UNLOCKED 🔓", unsafe_allow_html=True)
+         st.markdown("<h1 style='text-align: center; margin-bottom: 0.5rem; color: #000000;'> UNLOCKED 🔓", unsafe_allow_html=True)
       st.write("You Just started. Achieve:", f"{total_hours_completed:.1f}/0 hours to get this badge")
 
 # Auto-refresh for live clock (every 1 second)
