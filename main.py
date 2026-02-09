b0a931615f4aeeee8e1045ea6430acd81d387d13
lau-kai-rui-gavin@s2023.ssts.edu.sg <157783000+LauKRGavin@users.noreply.github.com>
2026-02-09 23:07:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1082,7 +1082,7 @@ with tab6:
    st.subheader(f"**Hours of work done:** {total_hours_completed:.1f}h,**Total activities:** {total_activities} Activites,**Badges Earnt:** {Badge} Badges")
    col1, col2, col3 = st.columns(3)
    with col1:
-      if total_hours_completed >= 1:
+      if total_hours_completed >= 0:
          st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #FFFFFF;'>✅", unsafe_allow_html=True)
          st.markdown("<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #000000;'> UNLOCKED 🔓", unsafe_allow_html=True)
          Badge+=1
