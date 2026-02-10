ef9422af2d31c682653b722f9e74c61d4680c427
lau-kai-rui-gavin@s2023.ssts.edu.sg <157783000+LauKRGavin@users.noreply.github.com>
2026-02-10 22:06:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1113,6 +1113,37 @@ with tab6:
       st.write("Commitment! Achieve:", f"{total_hours_completed:.1f}/168 hours to get this badge")
    col4, col5, col6 = st.columns(3)
    with col4:
+      if total_activities >= 5:
+         st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #FFFFFF;'>💼", unsafe_allow_html=True)
+         st.markdown("<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #00FF00;'> UNLOCKED 🔓", unsafe_allow_html=True)
+         Badge+=1
+      else:
+         st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #000000;'>❌", unsafe_allow_html=True)
+         st.markdown(f"<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #FF0000;'>Please obtain {5 - total_activites} activites to obtain this badge.</h1>", unsafe_allow_html=True)
+
+      st.write("My first assignments! Achieve:", f"{total_activities:.1f}/5 activites to get this badge")
+   with col5:
+      if total_hours_completed >= 24:
+         st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #FFFFFF;'>📅", unsafe_allow_html=True)
+         st.markdown("<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #00FF00;'> UNLOCKED 🔓", unsafe_allow_html=True)
+         Badge+=1
+      else:
+         st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #000000;'>❌", unsafe_allow_html=True)
+         st.markdown(f"<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #FF0000;'>Please obtain {24 - total_hours_completed}h to obtain this badge.</h1>", unsafe_allow_html=True)
+
+      st.write("A day of work! Achieve:", f"{total_hours_completed:.1f}/24 hours to get this badge")
+   with col6:
+      if total_hours_completed >= 168:
+         st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #FFFFFF;'>👍", unsafe_allow_html=True)
+         st.markdown("<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #00FF00;'> UNLOCKED 🔓", unsafe_allow_html=True)
+         Badge+=1
+      else:
+         st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #000000;'>❌", unsafe_allow_html=True)
+         st.markdown(f"<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #FF0000;'>Please obtain {168 - total_hours_completed}h to obtain this badge.</h1>", unsafe_allow_html=True)
+
+      st.write("Commitment! Achieve:", f"{total_hours_completed:.1f}/168 hours to get this badge")
+   col7, col8, col9 = st.columns(3)
+   with col4:
       if total_hours_completed >= 0:
          st.markdown("<h1 style='text-align: center; font-size: 10rem; color: #FFFFFF;'>✅", unsafe_allow_html=True)
          st.markdown("<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #00FF00;'> UNLOCKED 🔓", unsafe_allow_html=True)
@@ -1142,6 +1173,7 @@ with tab6:
          st.markdown(f"<h1 style='text-align: center; margin-bottom: 1rem; font-size: 1rem; color: #FF0000;'>Please obtain {168 - total_hours_completed}h to obtain this badge.</h1>", unsafe_allow_html=True)
 
       st.write("Commitment! Achieve:", f"{total_hours_completed:.1f}/168 hours to get this badge")
+      
 
 # Auto-refresh for live clock (every 1 second)
 st.markdown("""
