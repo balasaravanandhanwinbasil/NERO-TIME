2586fa989b038d8a70a60b1ccacd87e7e165328f
Fredchesco <145110816+Frankythefred@users.noreply.github.com>
2026-02-20 21:01:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ if not st.session_state.user_id:
 
 # ── MAIN APP ───────────────────────────────────────────────────────────────────
 st.markdown(
-    "<h1 style='text-align: center; margin-bottom: 7px; color: #E91E63;'>🕛 NERO-TIME</h1>",
+    "<h1 style='text-align: center; margin-bottom: 0px; color: #E91E63;'>🕛 NERO-TIME</h1>",
     unsafe_allow_html=True
 )
 #clock
